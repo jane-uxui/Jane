a792ae064e2d31a9e1a243b1150a82a88d5c7b2b
jane-uxui <janeyoon0127@gmail.com>
2025-11-24 16:31:54 +0900

    251124_update

--- MCard/script.js
+++ MCard/script.js
@@ -113,13 +113,13 @@
   window.onload = function() {
   var container = document.getElementById('map');
   var options = {
-    center: new kakao.maps.LatLng(33.450701, 126.570667),
+    center: new kakao.maps.LatLng(37.3815349667371, 126.6597361315122),
     level: 2
   };
   var map = new kakao.maps.Map(container, options);
 
   var marker = new kakao.maps.Marker({
-    position: new kakao.maps.LatLng(33.450701, 126.570667)
+    position: new kakao.maps.LatLng(37.3815349667371, 126.6597361315122)
   });
   marker.setMap(map);
 };
